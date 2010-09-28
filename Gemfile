f54e20e98582627b7b543cd85eca97fae7088e82
Andrea Longhi <andrea74@mailshack.com>
2010-09-28 22:49:37 +0200

    initial import

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'ruby-debug'
+gem 'devise'
+gem 'nokogiri'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'cucumber'
+  gem 'rspec-rails', '>= 2.0.0.beta.22'
+  gem 'spork'
+  gem 'launchy'
+  gem 'autotest'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whowonit::Application.config.secret_token = 'dfc8e01da3c9e95be1e71437be05b39a44357a2b76e678deddbfecfa2bfdd005ae53cc9b4df033109ec12852d49668be907449a09ff52020253cf976af0c14e7'
