b2f18560aa4bbbf76c98f95c1d996320c9e1d973
Andrea Longhi <andrea74@mailshack.com>
2010-09-30 14:20:30 +0200

    add some dummy test data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'factory_girl'
+require File.join(Rails.root, 'spec/factories')
+
+user = Factory(:user, :email => 'andrea@spaghetticode.it', :confirmed_at => Time.now)
+seller = Factory(:seller, :name => 'old.comp')
+buyer = Factory(:buyer, :name => 'superfiko')
+auction = Factory(:auction, :seller => seller, :buyer => buyer)
+auction.users << user
