a78f5869fd431766ecf4693612e35685f4a4d5e9
Andrea Longhi <andrea74@mailshack.com>
2010-10-03 10:21:17 +0200

    update ebayer model
    * add feedback_html method

--- app/models/ebayer.rb
+++ app/models/ebayer.rb
@@ -12,4 +12,14 @@ class Ebayer < ActiveRecord::Base
   def feedback_url
     "http://feedback.ebay.com/ws/eBayISAPI.dll?ViewFeedback2&userid=#{name}&items=200&ftab=FeedbackAsSeller&interval=30"
   end
+  
+  def feedback_html
+    uri = URI.parse(feedback_url)
+    request = Net::HTTP.new(uri.host, uri.port)
+    request.read_timeout = 5
+    begin
+      response = request.get(uri.request_uri)
+    end
+    response.body
+  end
 end
