2ecb55a72a567cfd57945e65b50e50aafaeb0348
Andrea Longhi <andrea74@mailshack.com>
2010-10-03 10:29:18 +0200

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 require 'factory_girl'
 require File.join(Rails.root, 'spec/factories')
 
+[User, Ebayer, Auction].each do |model|
+  model.delete_all
+end
+
 user = Factory(:user, :email => 'andrea@spaghetticode.it', :confirmed_at => Time.now)
-seller = Factory(:seller, :name => 'old.comp')
-buyer = Factory(:buyer, :name => 'superfiko')
+seller = Factory(:ebayer, :name => 'old.comp')
+buyer = Factory(:ebayer, :name => 'superfiko')
 auction = Factory(:auction, :seller => seller, :buyer => buyer)
 auction.users << user
