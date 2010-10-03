e78c01e7c84ac83e23b541894687610d309b3721
Andrea Longhi <andrea74@mailshack.com>
2010-10-03 20:50:06 +0200

    add profile_for helper method

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -2,4 +2,9 @@ module AuctionsHelper
   def gallery_image_tag(auction)
     image_tag "http://thumbs3.ebaystatic.com/pict/#{auction.item_id}.jpg"
   end
+  
+  def profile_for(ebayer)
+    return '-' unless ebayer
+    link_to ebayer.name, ebayer.profile_url
+  end
 end
--- spec/helpers/auctions_helper_spec.rb
+++ spec/helpers/auctions_helper_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the AuctionsHelper. For example:
-#
-# describe AuctionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe AuctionsHelper do
-  describe 'gallery_image_tag' do
+  describe 'GALLERY_IMAGE_TAG' do
     before { @auction = Factory.stub(:auction) }
     
     it 'should return an image tag' do
-      helper.gallery_image_tag(@auction).should =~ %r[<img.+/>]
+      helper.gallery_image_tag(@auction).should =~ %r[^<img.+/>$]
     end
     
     it 'should include expected string' do
       expected = "http://thumbs3.ebaystatic.com/pict/#{@auction.item_id}.jpg"
       helper.gallery_image_tag(@auction).should include(expected)
+    end
+  end
+  
+  describe 'PROFILE_FOR' do
+    before { @ebayer = Factory.stub(:ebayer) }
+    
+    it 'should return a link tag' do
+      helper.profile_for(@ebayer).should =~ %r[^<a href=".+</a>$]
+    end
+    
+    it 'should include the expected url' do
+      helper.profile_for(@ebayer).should include(@ebayer.profile_url)
     end
   end
 end
