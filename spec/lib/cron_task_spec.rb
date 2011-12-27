f5102bd023183aa2395d655eb81c5080e07cbdd1
andrea longhi <andrea@spaghetticode.it>
2011-12-27 15:48:03 +0100

    moved cron spec to spec/lib folder

--- spec/lib/cron_task_spec.rb
+++ spec/lib/cron_task_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'rake'
 
 describe 'cron task' do
-  
   def prepare_rake
     rake = Rake::Application.new
     Rake.application = rake
@@ -10,7 +9,7 @@ describe 'cron task' do
     Rake::Task.define_task(:environment)
     @rake_cron = rake['cron']
   end
-  
+
   def stub_feeback_html(file)
     URI.stub!(:parse => mock.as_null_object)
     body = mock(:body => File.read(file))
@@ -18,37 +17,37 @@ describe 'cron task' do
     response.stub!(:get => body)
     Net::HTTP.stub!(:new => response)
   end
-  
-  def create_auctions  
+
+  def create_auctions
     @open      = Factory(:auction, :item_id => '380261770710')
     @pending   = Factory(:auction, :item_id => '130423805348', :end_time => 1.day.ago)
     @invisible = Factory(:auction, :item_id => '380267241870', :end_time => 91.days.ago)
     @unpending = Factory(:auction, :item_id => '130429054922', :end_time => 1.day.ago, :buyer => Factory(:ebayer))
   end
-  
+
   before do
     prepare_rake
     stub_feeback_html("#{Rails.root}/spec/fixtures/feedback.html")
     create_auctions
   end
-  
+
   it 'should not set @invisible buyer' do
     @rake_cron.invoke
     @invisible.reload.buyer.should be_nil
   end
-  
+
   it 'should not set @open buyer' do
     @rake_cron.invoke
     @open.reload.buyer.should be_nil
   end
-  
+
   it 'should not change @unpending buyer' do
     lambda do
       @rake_cron.invoke
       @unpending.reload
     end.should_not change(@unpending, :buyer)
   end
-  
+
   it 'should set expected buyer' do
     @rake_cron.invoke
     @pending.reload.buyer.should_not be_nil
